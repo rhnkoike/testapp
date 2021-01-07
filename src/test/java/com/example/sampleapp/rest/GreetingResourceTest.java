7c9f314a89ab785ddb96d627690442df0adcdaf7
nkoike <nkoike@redhat.com>
2021-01-07 15:04:59 +0900

    mod pipeline.md

--- src/test/java/com/example/sampleapp/rest/GreetingResourceTest.java
+++ src/test/java/com/example/sampleapp/rest/GreetingResourceTest.java
@@ -12,30 +12,7 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTestResource(TestStubService.class)
 public class GreetingResourceTest {
 
-    // @Inject
-    // @RestClient
-    // GreetingService greetingService;
-
-    // @Inject
-    // @ConfigProperty(name = "mock.externalservice.enabled", defaultValue = "false")
-    // Boolean mockExternalServiceEnabled;
-
-    // @BeforeAll
-    // public static void setup() {
-    //     // GreetingService mock = Mockito.mock(GreetingService.class);
-    //     // Mockito.when(mock.hello()).thenReturn("Hi ");
-    //     // QuarkusMock.installMockForType(new GreetingMockService(), GreetingService.class);
-    // }
-
-    // @BeforeEach
-    // public void setup(){
-        // if (mockExternalServiceEnabled) {
-            // Mockito.when(greetingService.hello()).thenReturn("hello from mockito");
-            // QuarkusMock.installMockForInstance(new GreetingMockService(), greetingService);
-            // System.out.println("mocking external service");
-        // }
-    // }
-
+ 
     @Test
     public void testGreetingEndpoint() {
      
